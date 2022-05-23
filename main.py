a2e50eb9ce9c6e644f974961d5b85dba1cceece1
AnzhelikaKurnikova <anzhelika.kurnikova@vicuesoft.com>
2022-05-23 16:39:35 +0300

    feaT: reduce the snake

--- main.py
+++ main.py
@@ -27,11 +27,21 @@ class SnakeGameClass():
         self.currentLength += distance
         self.previousHead = cx, cy
 
+        # Length Reduction
+        if self.currentLength > self.allowedLength:
+            for i, length in enumerate(self.lengths):
+                self.currentLength -= length
+                self.lengths.pop(i)
+                self.points.pop(i)
+                if self.currentLength < self.allowedLength:
+                    break
+
         # Draw snake
-        for i, point in enumerate(self.points):
-            if i != 0:
-                cv2.line(imgMain, self.points[i - 1], self.points[i], (0, 0, 255), 20)
-        cv2.circle(img, self.points[- 1], 20, (200, 0, 200), cv2.FILLED)
+        if self.points:
+            for i, point in enumerate(self.points):
+                if i != 0:
+                    cv2.line(imgMain, self.points[i - 1], self.points[i], (0, 0, 255), 20)
+            cv2.circle(img, self.points[-1], 20, (200, 0, 200), cv2.FILLED)
         return imgMain
 
 game = SnakeGameClass()
